fe41c22b85b0ee29386ffc2c0efb01d9c768201c
Chenxiaobo0828 <chenxiaobo0828@163.com>
2025-08-04 19:02:36 +0800

    third commit

--- main_swin.py
+++ main_swin.py
@@ -6,10 +6,12 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from torchvision import transforms
 from utils import GradCAM, show_cam_on_image, center_crop_img
-from swin_model import swin_tiny_patch4_window7_224 as create_model
-from skimage import io, exposure, img_as_uint, img_as_float
+from swim_model import swin_tiny_patch4_window7_224 as create_model
+from skimage import io, exposure
 import SimpleITK
 import cv2
+
+
 class ResizeTransform:
     def __init__(self, im_h: int, im_w: int):
         self.height = self.feature_size(im_h)
@@ -36,6 +38,7 @@ class ResizeTransform:
 
         return result
 
+
 def center_crop_img(img: np.ndarray, size: int):
     h, w, c = img.shape
 
@@ -55,16 +58,15 @@ def center_crop_img(img: np.ndarray, size: int):
 
     if new_w == size:
         h = (new_h - size) // 2
-        img = img[h: h+size]
+        img = img[h: h + size]
     else:
         w = (new_w - size) // 2
-        img = img[:, w: w+size]
+        img = img[:, w: w + size]
 
     return img
 
-def main(path,savepath,target_category,weights_path,load):
-    # 注意输入的图片必须是32的整数倍
-    # 否则由于padding的原因会出现注意力飘逸的问题
+
+def main(path, savepath, target_category, weights_path, load):
     img_size = 224
     assert img_size % 32 == 0
 
@@ -76,16 +78,16 @@ def main(path,savepath,target_category,weights_path,load):
     target_layers = [model.norm]
 
     data_transform = transforms.Compose([transforms.ToTensor(),
-                                   transforms.Resize(256),
-                                   transforms.CenterCrop(img_size),
-                                   transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
-                                   ])
+                                         transforms.Resize(256),
+                                         transforms.CenterCrop(img_size),
+                                         transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+                                         ])
     # load image
     img_path = path
     assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
-    if load==0:
+    if load == 0:
         img = io.imread(img_path)[:, :, :3]
-    if load==1:
+    if load == 1:
         img = SimpleITK.ReadImage(img_path)
         img = SimpleITK.GetArrayFromImage(img)
         img = img[int(img.shape[0] / 2 - 1):int(img.shape[0] / 2 + 2), :, :]
@@ -93,12 +95,11 @@ def main(path,savepath,target_category,weights_path,load):
 
     img = img.astype(np.float32)
     img = exposure.rescale_intensity(img, out_range="float32")
-    rat = img.shape[1]/256
-    newhigh = int(img.shape[0]/rat)
-    newimg = cv2.resize(img, dsize=(256,newhigh))
-    higrat = int((newhigh-224)/2)
-    newimg = newimg[higrat:higrat+224,16:240]
-
+    rat = img.shape[1] / 256
+    newhigh = int(img.shape[0] / rat)
+    newimg = cv2.resize(img, dsize=(256, newhigh))
+    higrat = int((newhigh - 224) / 2)
+    newimg = newimg[higrat:higrat + 224, 16:240]
 
     # [C, H, W]
     img_tensor = data_transform(img)
@@ -114,38 +115,37 @@ def main(path,savepath,target_category,weights_path,load):
     grayscale_cam = cam(input_tensor=input_tensor, target_category=target_category)
 
     grayscale_cam = grayscale_cam[0, :]
-    visualization = show_cam_on_image(newimg, grayscale_cam,use_rgb=True)
-    if load==1:
-        visualization = np.flip(visualization, axis=1)#镜面翻转
-        visualization = np.rot90(visualization,1)#向左旋转
+    visualization = show_cam_on_image(newimg, grayscale_cam, use_rgb=True)
+    if load == 1:
+        visualization = np.flip(visualization, axis=1)
+        visualization = np.rot90(visualization, 1)
 
-    plt.imsave(savepath,visualization)
-    #plt.imshow(visualization)
-    #plt.show()
-
-path = r"/mnt/d/kneesoft/bulaiying.nii.gz" #病人的数据路径,具体为 xxxx/xx.nii.gz
-motai = "COR_MPR" #病人的序列
-label = 1 #想要绘制的class
-
-savepath = r"/mnt/d/kneesoft/testCAM" #CAM的保存路径
+    plt.imsave(savepath, visualization)
+    # plt.imshow(visualization)
+    # plt.show()
 
 
-if motai == "Xray": #判断模态
-   weights_path ="/mnt/d/kneesoft/weights/wXRAY.pth"
+path = r"/mnt/d/kneesoft/bulaiying.nii.gz"
+motai = "COR_MPR"
+label = 1
+
+savepath = r"/mnt/d/kneesoft/testCAM"
+
+if motai == "Xray":
+    weights_path = "./weights/wXRAY.pth"
 if motai == "SAG_IW_TSE":
-   weights_path = "/mnt/d/kneesoft/weights/wSAG.pth"
+    weights_path = "./weights/wSAG.pth"
 if motai == "COR_MPR":
-   weights_path = "/mnt/d/kneesoft/weights/wCOR.pth"
+    weights_path = "./weights/wCOR.pth"
 
-
-if os.path.exists(os.path.join(savepath,motai)) is False:
-    os.makedirs(os.path.join(savepath,motai))
+if os.path.exists(os.path.join(savepath, motai)) is False:
+    os.makedirs(os.path.join(savepath, motai))
 name = os.path.split(path)[1]
 
 if motai == "Xray":
-    thesave = os.path.join(savepath,motai,name[:-4]+".jpg")#输入为jpg格式的Xray，非DICOM
-    main(path,thesave,label,weights_path,load=0)
+    thesave = os.path.join(savepath, motai, name[:-4] + ".jpg")  # jpg
+    main(path, thesave, label, weights_path, load=0)
 else:
-    thesave = os.path.join(savepath,motai,name[:-7] + ".jpg") #输入形式为 .nii.gz
-    main(path, thesave,label, weights_path,load=1)
+    thesave = os.path.join(savepath, motai, name[:-7] + ".jpg")  # .nii.gz
+    main(path, thesave, label, weights_path, load=1)
 
--- predict.py
+++ predict.py
@@ -6,8 +6,8 @@ from PIL import Image
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import SimpleITK
-from model import swin_tiny_patch4_window7_224 as create_model
-from skimage import io, exposure, img_as_uint, img_as_float
+from swim_model import swin_tiny_patch4_window7_224 as create_model
+from skimage import io, exposure
 import numpy as np
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -20,68 +20,40 @@ def main():
                                    ])
 
     # read class_indict
-    json_path = './class_indices.json'
-    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
-
-    with open(json_path, "r") as f:
-        class_indict = json.load(f)
-
     # create model
     model = create_model(num_classes=2).to(device)
     # load model weights
-    model_weight_path = "/mnt/f/0.原始数据/0.code/swintransformerCOR/weightsCOR9/model-9,valauc0.893,valacc0.782,testauc0.785,testacc0.799.pth"
-    model.load_state_dict(torch.load(model_weight_path, map_location=device))
-
+    model_weight_path = r"C:\Users\Englishday\Desktop\0706knee\github_knee\weights\wCOR.pth"
+    model.load_state_dict(torch.load(model_weight_path, map_location=device), strict=False)
 
     model.eval()
 
     # load image
-    image_path = r"/mnt/f/0.原始数据/0.originNiidata/valdata/COR_MPR"
-    savepath = r"/mnt/f/1.clinical"
+    image_path = r"C:\Users\Englishday\Desktop\0706knee\International test data from OAI"
+    savepath = r"C:\Users\Englishday\Desktop\0706knee\save"
     assert os.path.exists(image_path), "file: '{}' dose not exist.".format(image_path)
     list_data = pd.DataFrame(columns=["ID", "Proba"])
     for i in os.listdir(image_path):
         for j in os.listdir(os.path.join(image_path,i)):
             for k in os.listdir(os.path.join(image_path, i,j)):
-                #img = SimpleITK.ReadImage(r"E:\data\AX\4\9000622left25.nii.gz")
                 img = SimpleITK.ReadImage(os.path.join(image_path,i,j,k))
                 img = SimpleITK.GetArrayFromImage(img)
                 img = img.transpose(2, 1, 0)
-                #img = io.imread(os.path.join(image_path, i,j,k))
                 img = img.astype(np.float32)
                 img = exposure.rescale_intensity(img, out_range="float32")
-                #plt.imshow(img)
-
-                # [N, C, H, W]
                 img = data_transform(img)
-                # expand batch dimension
                 img = torch.unsqueeze(img, dim=0)
 
                 with torch.no_grad():
                     # predict class
                     output = torch.squeeze(model(img.to(device))).cpu()
-
-                    #toPIL = transforms.ToPILImage()  # 这个函数可以将张量转为PIL图片，由小数转为0-255之间的像素值
-                    #pic = toPIL(output)
-                    #pic.save(r'C:\Users\Hongj\Downloads\random.jpg')
-
-
-
                     predict = torch.softmax(output, dim=0)
-                    predict_cla = torch.argmax(predict).detach().numpy()
 
                     list_newdata = pd.DataFrame({"ID": [k[:-7]], "0": [predict[0].detach().numpy()], "1": [predict[1].detach().numpy()]})
                     list_data = list_data._append(list_newdata)
 
-                list_data.to_csv(os.path.join(savepath, "预测概率表COR.csv"),encoding="gbk", index=False)
+                list_data.to_csv(os.path.join(savepath, "COR.csv"),encoding="gbk", index=False)
 
-            #print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
-                                                        # predict[predict_cla].numpy())
-            #plt.title(print_res)
-            #for i in range(len(predict)):
-                #print("class: {:10}   prob: {:.3}".format(class_indict[str(i)],
-                                                         # predict[i].numpy()))
-            #plt.show()
 
 if __name__ == '__main__':
     main()
